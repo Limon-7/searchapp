ec1cde8842051247e21bce3b9e7a51f6c679a000
Limon-7 <liibd7@gmail.com>
2020-01-03 14:39:25 +0600

    Working with multiple Observale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchService } from './_service/search.service';
@@ -9,6 +10,7 @@ import { appRoutes } from './_routing/route';
 import { CorePromiseComponent } from './http/core-promise/core-promise.component';
 import { CoreObservableComponent } from './http/core-observable/core-observable.component';
 import { JsonpObservableComponent } from './http/jsonp-observable/jsonp-observable.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { JsonpObservableComponent } from './http/jsonp-observable/jsonp-observab
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule
 
   ],
   providers: [
--- src/app/http/core-observable/core-observable.component.ts
+++ src/app/http/core-observable/core-observable.component.ts
@@ -2,27 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { SearchIteam } from 'src/app/_model/searchIteam';
 import { SearchService } from 'src/app/_service/search.service';
 import { SearchByInterface } from 'src/app/_model/searchByInterface';
-
+import { Observable, Subscriber } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, subscribeOn, map, switchMap, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-core-observable',
   templateUrl: './core-observable.component.html',
   styleUrls: ['./core-observable.component.css']
 })
 export class CoreObservableComponent implements OnInit {
-
-  results: SearchIteam;
-  // results: SearchByInterface[];
   loading = false;
-  constructor(private searchService: SearchService) { }
+  results: Observable<SearchIteam>;
+  searchField: FormControl;
+  // results: SearchByInterface[];
 
+  constructor(private searchService: SearchService) { }
+  /*
+  .the valueChanges obserable is emmiting the type of  obserable<string>
+  .
+  .
+  .
+  .the searchByObservable function  from our search service that is of type Obserable<Searchiteam>
+  */
   ngOnInit() {
+    this.searchField = new FormControl();
+    this.results = this.searchField.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(() => this.loading = true),
+      /* isn’t returning SearchItem[] it’s returning Observable<SearchItem[]>.
+      So the map operator isn’t converting a string to SearchItem[] it’s converting a string to Observable<SearchItem[]>
+      So the subscribe function is receiving Observable<SearchItem[]> and not SearchItem[] as we want.
+      One workaround would then be to just try doing two subscribes that means something like observable<Obserable<Searchiteam>>
+      */
+      switchMap(term => this.searchService.searchByObservable(term)),
+      tap(() => this.loading = false)
+    );
+    /* // Now that our observable chain returns Observable<SearchItem[]> we can just assign it to our local results property
+    .subscribe(val => {
+      console.log(val);
+    }); */
   }
   searchByObservable(term: string) {
+    this.searchService.searchByObservable(term);
+  }
+  /* using async pipe */
+  /* searchByObservable(term: string) {
+    this.loading = true;
+    this.results = this.searchService.searchByObservable(term);
+  } */
+  /* using subscription */
+  /* searchByObservable(term: string) {
     this.loading = true;
     this.searchService.searchByObservable(term).subscribe(data => {
       console.log(data);
       this.results = data;
       this.loading = false;
     });
-  }
+  } */
 }
